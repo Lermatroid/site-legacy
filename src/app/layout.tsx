9e377daa796c18bbd07f0d934351fadb87342aca
Liam Murray <liam@murray.to>
2024-04-08 10:06:37 -0500

    Add site name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
+export const metadata: Metadata = {
+	title: "Liam Murray",
+};
+
 export default function RootLayout({
 	children,
 }: Readonly<{
