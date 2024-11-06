df6fcc73b9bc92ae8e924438b1b3c18e80c79858
Liam Murray <liam@murray.to>
2024-11-06 05:29:56 -0600

    Add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/bsky",
+				destination: "https://bsky.app/profile/liam.so",
+				permanent: false,
+			},
+			{
+				source: "/twitter",
+				destination: "https://x.com/lermatroid",
+				permanent: false,
+			},
+			{
+				source: "/x",
+				destination: "https://x.com/lermatroid",
+				permanent: false,
+			},
+			{
+				source: "/github",
+				destination: "https://github.com/lermatroid",
+				permanent: false,
+			},
+			{
+				source: "/gh",
+				destination: "https://github.com/lermatroid",
+				permanent: false,
+			},
+			{
+				source: "/instagram",
+				destination: "https://www.instagram.com/lermatroid",
+				permanent: false,
+			},
+			{
+				source: "/ig",
+				destination: "https://www.instagram.com/lermatroid",
+				permanent: false,
+			},
+			{
+				source: "/instagram",
+				destination: "https://www.instagram.com/lermatroid",
+				permanent: false,
+			},
+			{
+				source: "/linkedin",
+				destination: "https://linkedin.com/in/liamrmurray",
+				permanent: false,
+			},
+		];
+	},
+};
 
 export default nextConfig;
