803e77dc1b4a2da269c8915628393126d257a20b
Liam Murray <liam@murray.to>
2024-12-31 18:04:41 -0600

    Add suspense to spotify bubble

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Github, Linkedin, Instagram } from "lucide-react";
 import GitHubCalendar from "react-github-calendar";
 import SpotifyBubble from "@/components/spotify-bubble";
+import { Suspense } from "react";
 
 export default function Home() {
 	return (
@@ -144,38 +145,10 @@ export default function Home() {
 					</Link>
 				</div>
 				<div className="col-start-1 row-start-4 rounded-2xl bg-zinc-950 p-5">10</div>
-				<SpotifyBubble />
+				<Suspense>
+					<SpotifyBubble />
+				</Suspense>
 			</section>
-			{/* <section className="h-screen w-screen p-10" id="about">
-				<div className="w-full h-full p-10 pt-80 pb-40 flex flex-col justify-center gap-y-10">
-					<h1 className="font-gmono font-black text-white text-8xl">About</h1>
-					<div className="grid grid-cols-2 w-full gap-x-4">
-						<div className="rounded-xl border-white border bg-black w-full aspect-video p-5 flex items-center">
-							<p className="font-mono font-bold text-xl leading-normal">
-								Hey there!
-								<br />
-								<br />
-								My name's Liam. I'm a sofware engineer based out of San Antonio. I'm
-								currently studying Computer Science at the University of Texas at
-								San Antonio while working on the side as a freelance software
-								engineer. During my freetime I enjoy playing games, working on side
-								projects, and walking.
-								<br />
-								<br />
-								You can find me around the internet over here &rArr;
-							</p>
-						</div>
-						<SocialWrapper />
-					</div>
-				</div>
-			</section> */}
-			{/* <ProjectsViewer />
-			<section className="w-screen p-10 bg-black grid grid-cols-2">
-				<div className="min-h-screen flex items-center justify-center">
-					<h1 className="font-black font-mono text-7xl">Writings</h1>
-				</div>
-				<div className="min-h-screen flex items-center justify-center"></div>
-			</section> */}
 		</>
 	);
 }
